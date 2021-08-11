1a41781be0984cdfe32322b2b8788f840a90ba7e
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-08-11 10:52:59 +0100

    refactor: delete unnecessary files

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,11 @@ import generateSalt from './generate-salt';
 import { createHash, validateHash } from './hash';
 import generateApiKey from './generate-api-key';
 import generateRandomString from './generate-random-chars';
-import {
-  IGenerateApiKey,
-  IGenerateSalt,
-  ICreateHash,
-  ValidateHashClosure,
-  IGenerateRandomString,
-} from './types';
-
 
 export default {
   generateSalt,
   createHash,
   generateApiKey,
-  validateHash: ({ hashValue, valueToCompare }: ValidateHashClosure): Promise<boolean> =>
-    validateHash({ hashValue, valueToCompare }),
-  generateRandomString: ({ length }: IGenerateRandomString) => generateRandomString({ length }),
+  validateHash,
+  generateRandomString,
 }
